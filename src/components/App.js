1b5c7bc4ce2f9994a2baee0f5f2e9637832a0257
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-24 01:57:16 -0700

    Add App component with sticky header and imported components

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Header from './Header/Header';
+import Main from './Main/Main';
+// import Footer from './Footer/Footer';
+import Container from '@material-ui/core/Container';
+
+const pageStyles = {
+  margin: 'auto',
+  padding: 0,
+  minHeight: '100vh',
+  backgroundColor: '#FCF7F5'
+}
+
+const stickyHeader = {
+  position: 'sticky',
+  top: 0,
+  width: '100%',
+  zIndex: 1,
+}
+
+export default function App() {
+  return (
+    <>
+      <Container maxWidth='xl' style={pageStyles}>
+        <div style={stickyHeader}>
+        <Header />
+        </div>
+        <Main />
+        {/* <Footer /> */}
+      </Container>
+    </>
+  );
+}
