9badad1e0fff64a08d66a7d60781fa86e19e9095
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-24 00:56:36 -0700

    Add Header with imported Promo and Navigation components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Promo from './Promo';
+import Navigation from './Navigation';
+
+export default function Header() {
+  return (
+    <>
+      <Promo />
+      <Navigation />
+    </>
+  );
+}
