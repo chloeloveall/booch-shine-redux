9ce8a28d8c293a06e6bc3d86f356aba06dd47b75
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-25 11:54:19 -0700

    Update styles/color scheme, add two additional carousel images

--- src/components/Main/ImageCarousel.js
+++ src/components/Main/ImageCarousel.js
@@ -7,6 +7,8 @@ import Carousel3 from '../../img/carousel3.png';
 import Carousel4 from '../../img/carousel4.png';
 import Carousel5 from '../../img/carousel5.png';
 import Carousel6 from '../../img/carousel6.png';
+import Carousel7 from '../../img/carousel7.png';
+import Carousel8 from '../../img/carousel8.png';
 
 const useStyles = makeStyles(() => ({
   carouselStyle: {
@@ -19,8 +21,8 @@ const useStyles = makeStyles(() => ({
     paddingTop: 0,
   },
   shadow: {
-    filter: 'drop-shadow(0 0 8px rgba(0, 0, 0, 1))',
-    paddingBottom: '2em',
+    filter: 'drop-shadow(0 0 10px #000000)',
+    paddingBottom: '5em',
   },
   carouselMax: {
     maxHeight: '400px',
@@ -98,6 +100,28 @@ export default function ImageCarousel() {
 
         <Carousel.Item>
           <img className='d-block w-100' src={Carousel6} alt='Fifth slide' />
+
+          {/* <Carousel.Caption className={classes.carouselCaptions}>
+            <h3>Fifth slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className='d-block w-100' src={Carousel7} alt='Seventh slide' />
+
+          {/* <Carousel.Caption className={classes.carouselCaptions}>
+            <h3>Fifth slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className='d-block w-100' src={Carousel8} alt='Eighth slide' />
 
           {/* <Carousel.Caption className={classes.carouselCaptions}>
             <h3>Fifth slide label</h3>
