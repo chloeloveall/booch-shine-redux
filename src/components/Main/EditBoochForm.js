c1dd0452c009111f3b1d63b872d9ea983c637e0a
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-26 18:53:35 -0700

    Add EditBoochForm

---
+++ src/components/Main/EditBoochForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+export default function EditBoochForm (props) {
+
+  const { booch } = props;
+
+  function handleEditBoochFormSubmission(event) {
+    event.preventDefault();
+    props.onEditBooch({
+      name: event.target.name.value, 
+      brand: event.target.brand.value, 
+      price: event.target.price.value,
+      alcoholContent: event.target.alcoholContent.value,
+      flavorDescription: event.target.flavorDescription.value,
+      // remainingPints: event.target.remainingPints.value,  
+      id: booch.id
+    });
+  }
+
+  return (
+    <>
+      <ReusableForm
+        formSubmissionHandler={handleEditBoochFormSubmission}
+        buttonText="Update Booch" />
+    </>
+  );
+}
+
+EditBoochForm.propTypes = {
+  booch: PropTypes.object,
+  onEditBooch: PropTypes.func
+};
