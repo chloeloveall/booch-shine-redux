1e08b9c9ec110fffe588da521740fe0c2925ed52
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-27 01:26:44 -0700

    Style changes to remove compile errors

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,11 +4,11 @@ import Logo from './Logo';
 import ImageCarousel from './ImageCarousel';
 import BoochControl from './BoochControl';
 
-const pageStyles = {
-  backgroundColor: '#F8D294',
-  margin: 'auto',
-  filter: 'drop-shadow(0 7px 3px rgba(0, 0, 0, .4))',
-}
+// const pageStyles = {
+//   backgroundColor: '#F8D294',
+//   margin: 'auto',
+//   filter: 'drop-shadow(0 7px 3px rgba(0, 0, 0, .4))',
+// }
 
 const test = {
   backgroundColor: '#ffffff',
@@ -18,7 +18,7 @@ const test = {
 export default function Main() {
   return (
     <>
-      <div style={pageStyles, {backgroundColor: '#000000', backgroundImage: `url(${DarkBackground})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+      <div style={{backgroundColor: '#000000', backgroundImage: `url(${DarkBackground})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
         <Logo />
         <ImageCarousel />
         <div style={test}>
