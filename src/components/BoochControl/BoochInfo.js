5729576cfbe952812c8eb1253aa81bc14654fb32
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-30 16:01:20 -0700

    Rewrite functional component to improve site performance

--- src/components/BoochControl/BoochInfo.js
+++ src/components/BoochControl/BoochInfo.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
-export default function BoochInfo() {
+const BoochInfo = () => {
+// export default function BoochInfo() {
   return (
     <>
       <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
@@ -14,3 +15,5 @@ export default function BoochInfo() {
     </>
   );
 }
+
+export default BoochInfo;
