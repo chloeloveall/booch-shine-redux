5dd3b57668184ce6250e1cacba88e5204d4ed132
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-30 09:47:22 -0700

    Remove BoochControl from Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,18 +2,17 @@ import React from 'react';
 import DarkBackground from '../../img/darkbackground.png';
 import Logo from './Logo';
 import ImageCarousel from './ImageCarousel';
-import BoochControl from './BoochControl';
 
 // const pageStyles = {
 //   backgroundColor: '#F8D294',
 //   margin: 'auto',
 //   filter: 'drop-shadow(0 7px 3px rgba(0, 0, 0, .4))',
-// }
+// } 
 
-const test = {
-  backgroundColor: '#ffffff',
-  height: '500px',
-}
+// const test = {
+//   backgroundColor: '#ffffff',
+//   height: '500px',
+// }
 
 export default function Main() {
   return (
@@ -21,9 +20,6 @@ export default function Main() {
       <div style={{backgroundColor: '#000000', backgroundImage: `url(${DarkBackground})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
         <Logo />
         <ImageCarousel />
-        <div style={test}>
-          <BoochControl />
-        </div>
       </div>
     </>
   );
