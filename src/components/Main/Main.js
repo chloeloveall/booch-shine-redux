524f27c86554ead3a3712cd19df621a6c3750f82
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-30 20:43:13 -0700

    Add bottom border

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -18,7 +18,7 @@ const Main = () => {
 // export default function Main() {
   return (
     <>
-      <div style={{backgroundColor: '#000000', backgroundImage: `url(${DarkBackground})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+      <div style={{backgroundColor: '#000000', backgroundImage: `url(${DarkBackground})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', borderBottom: '2px #f2be40 solid', filter: 'drop-shadow(0 7px 3px rgba(0, 0, 0, .4))'}}>
         <Logo />
         <ImageCarousel />
       </div>
