01475daeee832fe71483c32118f8a89751956b92
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-30 22:15:44 -0700

    Pass test to add new booch to mainBoochList

--- src/__tests__/reducers/booch-list-reducer.test.js
+++ src/__tests__/reducers/booch-list-reducer.test.js
@@ -1,7 +1,48 @@
 import boochListReducer from '../../reducers/booch-list-reducer';
 
 describe('boochListReducer', () => {
+
+  let action;
+  const boochData = {
+    name: 'Midnight Painkiller',
+    brand: 'Hard Kombucha',
+    price: '$7.99',
+    alcoholContent: '6%',
+    flavorDescription: 'Pineapple and charcoal',
+    remainingPints: '124',
+    // remainingPintsMessage: PropTypes.string,
+    id: 1,
+  };
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(boochListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new booch data to mainBoochList', () => {
+    const { name, brand, price, alcoholContent, flavorDescription, remainingPints, id } = boochData;
+    action = {
+      type: 'ADD_BOOCH',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      flavorDescription: flavorDescription,
+      remainingPints: remainingPints,
+      // remainingPintsMessage: PropTypes.string,
+      id: id,
+    };
+
+    expect(boochListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        flavorDescription: flavorDescription,
+        remainingPints: remainingPints,
+        // remainingPintsMessage: PropTypes.string,
+        id: id,
+      }
+    });
+  });
+
 });
--- src/reducers/booch-list-reducer.js
+++ src/reducers/booch-list-reducer.js
@@ -1,3 +1,20 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, alcoholContent, flavorDescription, remainingPints, id } = action;
+  switch (action.type) {
+  case 'ADD_BOOCH':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        flavorDescription: flavorDescription,
+        remainingPints: remainingPints,
+        // remainingPintsMessage: PropTypes.string,
+        id: id,
+      }
+    });
+  default:
+    return state;
+  }
 };
