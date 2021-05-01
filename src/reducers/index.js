340c5f1b761a1061ace22b4afbf729f8c60fd7fe
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-01 12:50:10 -0700

    Add root reducer and pass root reducer tests

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import formVisibleReducer from './form-visible-reducer';
+import boochListReducer from './booch-list-reducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  mainBoochList: boochListReducer
+});
+
+export default rootReducer;
