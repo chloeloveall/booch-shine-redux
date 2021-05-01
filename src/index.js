c40fca9c777dd8138fd8735d4d8bbb41e7b071fa
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-01 10:53:15 -0700

    Create store, import booch list reducer, add Provider

--- src/index.js
+++ src/index.js
@@ -7,11 +7,20 @@ import './index.css';
 import './components/App.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import reducer from './reducers/booch-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
+
+store.subscribe(() =>
+  console.log(store.getState())
+);
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>,
+  </Provider>,
   document.getElementById('root')
 );
 
