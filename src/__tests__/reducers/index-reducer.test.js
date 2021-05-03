9b82bd0650ce3631788ad6c93f8025004fa17d84
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-02 21:11:13 -0700

    Pass tests for selected booch reducer with root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import boochListReducer from '../../reducers/booch-list-reducer';
+import selectedBoochReducer from '../../reducers/selected-booch-reducer';
 import * as c from '../../actions/ActionTypes';
 
 let store = createStore(rootReducer)
@@ -11,20 +12,27 @@ describe('rootReducer', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       mainBoochList: {},
       formVisibleOnPage: false,
-    })
-  })
+      selectedBooch: null
+    });
+  });
 
   test('Check that initial state of boochListReducer matches root reducer', () => {
     expect(store.getState().mainBoochList).toEqual(
       boochListReducer(undefined, { type: null }),
-    )
-  })
+    );
+  });
 
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(
       formVisibleReducer(undefined, { type: null }),
-    )
-  })
+    );
+  });
+
+  test('Check that initial state of selectedBoochReducer matches root reducer', () => {
+    expect(store.getState().selectedBooch).toEqual(
+      selectedBoochReducer(null, { type: null }),
+    );
+  });
 
   test('Check that ADD_BOOCH action works for boochListReducer and root reducer', () => {
     const action = {
@@ -41,8 +49,8 @@ describe('rootReducer', () => {
     store.dispatch(action)
     expect(store.getState().mainBoochList).toEqual(
       boochListReducer(undefined, action),
-    )
-  })
+    );
+  });
 
   test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
     const action = {
@@ -51,6 +59,28 @@ describe('rootReducer', () => {
     store.dispatch(action)
     expect(store.getState().formVisibleOnPage).toEqual(
       formVisibleReducer(undefined, action),
-    )
-  })
-})
+    );
+  });
+
+  test('Check that SELECT_BOOCH action works for selectedBoochReducer and root reducer', () => {
+    const action = {
+      type: c.SELECT_BOOCH,
+      id: 1
+    }
+    store.dispatch(action)
+    expect(store.getState().selectedBooch).toEqual(
+      selectedBoochReducer(null, action),
+    );
+  });
+
+  test('Check that MAKE_NULL action works for selectedBoochReducer and root reducer', () => {
+    const action = {
+      type: c.MAKE_NULL
+    }
+    store.dispatch(action)
+    expect(store.getState().selectedBooch).toEqual(
+      selectedBoochReducer(1, action),
+    );
+  });
+
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import formVisibleReducer from './form-visible-reducer';
 import boochListReducer from './booch-list-reducer';
+import selectedBoochReducer from './selected-booch-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  mainBoochList: boochListReducer
+  mainBoochList: boochListReducer,
+  selectedBooch: selectedBoochReducer
 });
 
 export default rootReducer;
